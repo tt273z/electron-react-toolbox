5b6c1f2334338bab50228f5fd9a43eb305a96931
qim <m17854257603@163.com>
2019-02-20 16:58:27 +0800

    M console page

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,33 +1,66 @@
 import React, { Component } from 'react';
+import { Button, Row, Col } from 'antd'
 
 import 'codemirror/mode/xml/xml.js';
 import 'codemirror/mode/javascript/javascript.js'; 
 import { Controlled as CodeMirror } from 'react-codemirror2';
 
 class Console extends Component {
-  state = {
-    value: 'function StringStream(string) {  this.pos = 0;  this.string = string; }  '
+  constructor(props){
+    super(props)
+    this.state = {
+      code: '1+3'
+    }
+  }
+  run = () => {
+    let res = eval(this.state.code)
+    document.querySelector('iframe').contentDocument.write(res)
+  }
+  clearCode = () => {
+    this.setState({ code: '' })
   }
   render(){
-    let options = {
+    const options = {
       lineNumbers: false,//为true时样式错误?
       mode: 'javascript',
       theme: 'material',
       lineWrapping: true,
       readOnly: false,
-
+    }
+    const style = {
+      section: {
+        width: '50%',
+        float: 'left'
+      },
+      iframe: {
+        width: '100%',
+        height: 300,
+        border: 'none',
+        background: '#eee'
+      }
     }
     return (
-      <div style={{ width: 400 }}>
-        <CodeMirror 
-          value={ this.state.value }
-          options={ options } 
-          onChange={(editor,data, value) => {
-          }} 
-          onBeforeChange={(editor, data, value) => {
-            this.setState({value: value});
-          }}
-        />     
+      <div>
+        <div>
+          <Button type="primary" style={{ margin: '0 16px 16px 0' }} onClick={this.run}>运行</Button>
+          <Button type="primary" onClick={this.clearCode}>清空</Button>          
+        </div>
+        <Row>
+          <Col span={12}>
+            <CodeMirror 
+              value={ this.state.code }
+              options={ options } 
+              onChange={(editor,data, value) => {
+              }} 
+              onBeforeChange={(editor, data, value) => {
+                this.setState({code: value});
+              }}
+            />             
+          </Col>
+          <Col span={12}>
+            <iframe title="output" style={style.iframe}></iframe>
+          </Col>
+        </Row>
       </div>
     )
   }
