130ab49c7d5594610f65e58d38b93a0fa30c310b
mengQ99 <m17854257603@163.com>
2019-05-24 11:11:42 +0800

    firefox下textarea组件显示不正常问题fix

--- src/components/TemplateSyntax.js
+++ src/components/TemplateSyntax.js
@@ -25,29 +25,27 @@ export default class TemplateSyntax extends Component {
 
   render() {
     return (
-      <div className="templ-syntax full-h">
-        <Row type="flex" justify="space-between" align="middle" className="full-h">
-          <Col span={10} className="full-h">
-            <Button className="copy-icon" type="primary" shape="circle" icon="copy" size="large" onClick={() => copy2Clipboard(this.state.templStr)}/>
-            <TextArea className="full-h templ-box" 
-              value={this.state.templStr} 
-              onChange={e => this.setState({ templStr: e.target.value })} 
-              placeholder="# 输入模板语法"
-            />
-          </Col>
-          <Col span={2} style={{ textAlign: 'center' }}>
-            <Button type="primary" onClick={this.run}>Go <Icon type="right" /></Button>
-          </Col>
-          <Col span={10} className="full-h">
-            <Button className="copy-icon" type="primary" shape="circle" icon="copy" size="large" onClick={() => copy2Clipboard(this.state.normalStr)}/>
-            <TextArea className="full-h normal-box" 
-              value={this.state.normalStr} 
-              onChange={e => this.setState({ normalStr: e.target.value })} 
-              placeholder="# 输出普通字符串连接语法"
-            />
-          </Col>
-        </Row>
-      </div>
+      <Row type="flex" justify="space-between" align="middle" className="templ-syntax full-h">
+        <Col span={10} className="full-h">
+          <Button className="copy-icon" type="primary" shape="circle" icon="copy" size="large" onClick={() => copy2Clipboard(this.state.templStr)}/>
+          <TextArea className="full-h templ-box" 
+            value={this.state.templStr} 
+            onChange={e => this.setState({ templStr: e.target.value })} 
+            placeholder="# 输入模板语法"
+          />
+        </Col>
+        <Col span={2} style={{ textAlign: 'center' }}>
+          <Button type="primary" onClick={this.run}>Go <Icon type="right" /></Button>
+        </Col>
+        <Col span={10} className="full-h">
+          <Button className="copy-icon" type="primary" shape="circle" icon="copy" size="large" onClick={() => copy2Clipboard(this.state.normalStr)}/>
+          <TextArea className="full-h normal-box" 
+            value={this.state.normalStr} 
+            onChange={e => this.setState({ normalStr: e.target.value })} 
+            placeholder="# 输出普通字符串连接语法"
+          />
+        </Col>
+      </Row>
     )
   }
 }
